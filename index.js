230511f8daf6d8a6ed3d483e819d8ef81d456c7a
yining1023 <shiyining1023@gmail.com>
2022-09-20 12:13:26 -0400

    -

--- index.js
+++ index.js
@@ -24,9 +24,8 @@ const run = async () => {
         body = body.replace(el, "<" + url + "|" + txt + ">");
       }
     }
-    body = body.replaceAll("-", "•");
+    body = body.replaceAll("- ", "• ");
     body = body.replaceAll("**", "*");
-    console.log("body", body);
     const output = {
       blocks: [
         {
@@ -38,7 +37,6 @@ const run = async () => {
         },
       ],
     };
-    console.log("JSON.stringify(output)", JSON.stringify(output));
     core.setOutput("body", JSON.stringify(output));
   } catch (error) {
     core.setFailed(error.message);
