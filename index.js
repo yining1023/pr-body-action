0d4e5eb85425852c5fd6f2181f7b93b72894d0df
Christina Burger <pypmannetjies@gmail.com>
2023-10-10 17:07:12 -0400

    Fix regex for PRs with brackets in the name (#1)
    * Fix matching syntax for PRs with brackets in the name
    
    * Match the different parts correctly
    
    * Try rich text
    
    * Chunks
    
    * Formatting

--- index.js
+++ index.js
@@ -16,27 +16,37 @@ const run = async () => {
       pull_number: prNumber,
     });
     let body = pr.body;
-    let elements = body.match(/\[.*?\)/g);
+
+    // Ensure <> don't mess with links
+    body = body.replaceAll("<", "‹");
+    body = body.replaceAll(">", "›");
+
+    let elements = body.match(/\[.*\]\(.*\)/g);
     if (elements != null && elements.length > 0) {
       for (el of elements) {
-        let txt = el.match(/\[(.*?)\]/)[1]; //get only the txt
-        let url = el.match(/\((.*?)\)/)[1]; //get only the link
-        body = body.replace(el, "<" + url + "|" + txt + ">");
+        let txt = el.match(/\[(.*)\]/)[1]; //get only the txt
+        let url = el.match(/\]\((.*)\)/)[1]; //get only the link
+        body = body.replace(el, "<" + url.trim() + "|" + txt.trim() + ">");
       }
     }
-    body = body.replaceAll("- ", "• ");
+    body = body.replaceAll("^- ", "• ");
     body = body.replaceAll("**", "*");
+
+    // Split body into chunks of so that we don't reach the Slack API limit of 3000 characters
+    const bodyChunks = body.split("\n\n");
     const output = {
-      blocks: [
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: body,
-          },
-        },
-      ],
+      blocks: [],
     };
+    for (let i = 0; i < bodyChunks.length; i++) {
+      output.blocks.push({
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: bodyChunks[i],
+        },
+      });
+    }
+
     core.setOutput("body", JSON.stringify(output));
   } catch (error) {
     core.setFailed(error.message);
